e737deee95baf307564dc4a3c750dedc7b5d55a0
burperring <91236428+burperring@users.noreply.github.com>
2022-04-29 22:27:05 +0900

    Add files via upload

---
+++ Windows_Programming/API-OOP class 0.cpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProcedure(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("Class");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProcedure;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+//int px[1000], py[1000];
+
+typedef struct tagmyPOINT
+{
+	int  x;
+	int  y;
+} myPOINT;
+
+myPOINT p[1000];
+int iCount;   //점의 개수
+
+LRESULT CALLBACK WndProcedure(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{  //Window Procedure      //window handle(메시지가 발생한)
+	HDC hdc;
+	PAINTSTRUCT ps;
+	int x, y;
+	HBRUSH hBrush;
+	
+	switch (iMessage) {
+	case 0x0001://WM_CREATE:
+		hWndMain = hWnd;
+		return 0;
+	case  WM_LBUTTONDOWN: //왼쪽버튼을 눌렀을때
+		hdc = GetDC(hWnd);
+		//x = LOWORD(lParam);  //Macro
+		//x = (WORD)(lParam);  //casting  //형변환
+		x = (unsigned short)(lParam);  //unsigned short 16bit integer
+		//y = HIWORD(lParam);
+		y = (unsigned short)((lParam) >>16);
+	
+		p[iCount].x = x;
+		p[iCount].y = y;
+		iCount++;
+		InvalidateRect(hWnd, NULL, FALSE);
+		return 0;
+	
+	case WM_PAINT:  // 화면을 다시 그려라 os가 보내는 MSG
+		hdc = BeginPaint(hWnd, &ps);    //len = strlen(buf);  scanf("%d", &a);
+		// 화면복구 routine
+		hBrush = CreateSolidBrush(RGB(255, 0, 0));
+		SelectObject(hdc, hBrush);
+		for (int i = 0; i < iCount; i++)
+		{
+			Rectangle(hdc, p[i].x - 10, p[i].y - 10, 
+				p[i].x + 10, p[i].y + 10);
+		}
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
---
+++ Windows_Programming/API-OOP class 3.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProcedure(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("Class");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProcedure;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+//점들 - objects
+// description---> class : Abstraction 
+#include "points.h"  //Code reusability 코드재사용
+//실체화: instantiation
+mypoints points;  // 정의한 클래스의 (instance, object) 선언
+
+LRESULT CALLBACK WndProcedure(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{  //Window Procedure      //window handle(메시지가 발생한)
+	HDC hdc;
+	PAINTSTRUCT ps;
+	int x, y;
+	HBRUSH hBrush;
+	
+	
+	switch (iMessage) {
+	case 0x0001://WM_CREATE:
+		hWndMain = hWnd;
+		return 0;
+	case  WM_LBUTTONDOWN: //왼쪽버튼을 눌렀을때
+		hdc = GetDC(hWnd);
+		x = LOWORD(lParam);  //Macro
+		y = HIWORD(lParam);
+		points.Add(x, y);
+		InvalidateRect(hWnd, NULL, FALSE);
+		return 0;
+	
+	case WM_PAINT:  // 화면을 다시 그려라 os가 보내는 MSG
+		hdc = BeginPaint(hWnd, &ps);    //len = strlen(buf);  scanf("%d", &a);
+		// 화면복구 routine
+		points.Draw(hdc);
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
